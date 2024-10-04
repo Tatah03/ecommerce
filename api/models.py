7e0d380b3680f96b9550135e40b957e734cba899
Tatah03 <tamiresserra740@gmail.com>
2024-10-04 14:59:53 -0300

    V1

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Produto(models.Model):
+
+# Create your models here.
+    codigoProduto = models.CharField(max_length=255)
+    tituloProduto = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField(max_length=255)
+    imagemProduto = models.CharField(max_length=255)
+    categoriaProduto = models.CharField(max_length=255)
+    classificacaoProduto = models.DecimalField(max_digits=2, decimal_places=2)
+    exibirHome = models.BooleanField(default=True)
